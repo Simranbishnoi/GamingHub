605e7d8fd7b0d2018d0edbc99e7aae8dc78ccc0f
SimranBishnoi <simranbishnoi2121@gmail.com>
2026-04-01 21:57:47 +0530

    Fix routing bug with react-router-dom@6 & add .gitignore rules

--- gaminghub/src/App.js
+++ gaminghub/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './Dashboard';
+import RockPaperScissors from './RockPaperScissors';
+import BatBallStump from './BatBallStump';
+import GuessNumber from './GuessNumber';
 
 function App() {
   return (
-    <div>
-      <h1>Phase 1: Setup Complete!</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/rps" element={<RockPaperScissors />} />
+        <Route path="/batball" element={<BatBallStump />} />
+        <Route path="/guess" element={<GuessNumber />} />
+      </Routes>
+    </Router>
   );
 }
 
